4f57ca82aced12f5351644cefdd0f4e0773c1ba6
Eric Seidler <eric.seidler@gmail.com>
2019-09-05 11:45:44 -0500

    initial docker setup

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
@@ -7,14 +9,15 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 class UsersPing(Resource):
     def get(self):
         return {
             'status': 'success',
-            'message': 'pong!'
+            'message': 'pAng!'
         }
 
 
