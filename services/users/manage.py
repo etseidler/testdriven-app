be200ccc6359eb3dd9082044534528ef1e49d919
Eric Seidler <eric.seidler@gmail.com>
2019-09-10 10:19:52 -0500

    set up auth routes

--- services/users/manage.py
+++ services/users/manage.py
@@ -6,12 +6,7 @@ from project import create_app, db
 from project.api.models import User
 
 COV = coverage.coverage(
-    branch=True,
-    include='project/*',
-    omit=[
-        'project/tests/*',
-        'project/config.py',
-    ]
+    branch=True, include="project/*", omit=["project/tests/*", "project/config.py"]
 )
 COV.start()
 
@@ -19,25 +14,37 @@ app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
 
-@cli.command('recreate_db')
+@cli.command("recreate_db")
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
 
 
-@cli.command('seed_db')
+@cli.command("seed_db")
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(
+        User(
+            username="michael",
+            email="michael@reallynotreal.com",
+            password="greaterthaneight",
+        )
+    )
+    db.session.add(
+        User(
+            username="michaelherman",
+            email="michael@mherman.org",
+            password="greaterthaneight",
+        )
+    )
     db.session.commit()
 
 
 @cli.command()
 def test():
     """Runs the tests without code coverage"""
-    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    tests = unittest.TestLoader().discover("project/tests", pattern="test*.py")
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
@@ -47,12 +54,12 @@ def test():
 @cli.command()
 def cov():
     """Runs the unit tests with coverage."""
-    tests = unittest.TestLoader().discover('project/tests')
+    tests = unittest.TestLoader().discover("project/tests")
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         COV.stop()
         COV.save()
-        print('Coverage Summary:')
+        print("Coverage Summary:")
         COV.report()
         COV.html_report()
         COV.erase()
@@ -60,5 +67,5 @@ def cov():
     sys.exit(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
