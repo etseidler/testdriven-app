947f5c647cfb1724362ed3e563340cc742ecc0dc
Eric Seidler <eric.seidler@gmail.com>
2019-09-11 11:43:57 -0500

    add more e2e tests

--- services/client/src/components/__tests__/App.test.js
+++ services/client/src/components/__tests__/App.test.js
@@ -16,7 +16,7 @@ test('App renders without crashing', () => {
 
 test('App will call componentWillMount when mounted', () => {
   const onWillMount = jest.fn();
-  App.prototype.componentWillMount = onWillMount;
+  App.prototype.UNSAFE_componentWillMount = onWillMount;
   const wrapper = mount(<Router><App /></Router>);
   expect(onWillMount).toHaveBeenCalledTimes(1)
 });
