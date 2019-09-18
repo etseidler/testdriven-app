d7506b04350f7d374bc75c8747101be2b808d52d
Eric Seidler <eric.seidler@gmail.com>
2019-09-18 10:40:13 -0500

    add exercises db

--- services/exercises/manage.py
+++ services/exercises/manage.py
@@ -1,22 +1,15 @@
-# manage.py
-
-
 import sys
 import unittest
 
 import coverage
 from flask.cli import FlaskGroup
 
-from project import create_app
+from project import create_app, db  # new
+from project.api.models import Exercise  # new
 
 
 COV = coverage.coverage(
-    branch=True,
-    include='project/*',
-    omit=[
-        'project/tests/*',
-        'project/config.py',
-    ]
+    branch=True, include="project/*", omit=["project/tests/*", "project/config.py"]
 )
 COV.start()
 
@@ -27,7 +20,7 @@ cli = FlaskGroup(create_app=create_app)
 @cli.command()
 def test():
     """Runs the tests without code coverage"""
-    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    tests = unittest.TestLoader().discover("project/tests", pattern="test*.py")
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
@@ -37,12 +30,12 @@ def test():
 @cli.command()
 def cov():
     """Runs the unit tests with coverage."""
-    tests = unittest.TestLoader().discover('project/tests')
+    tests = unittest.TestLoader().discover("project/tests")
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         COV.stop()
         COV.save()
-        print('Coverage Summary:')
+        print("Coverage Summary:")
         COV.report()
         COV.html_report()
         COV.erase()
@@ -50,5 +43,13 @@ def cov():
     sys.exit(result)
 
 
-if __name__ == '__main__':
+# new
+@cli.command("recreate_db")
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+if __name__ == "__main__":
     cli()
--- services/exercises/project/tests/base.py
+++ services/exercises/project/tests/base.py
@@ -1,20 +1,19 @@
-# project/tests/base.py
-
-
 from flask_testing import TestCase
 
-from project import create_app
+from project import create_app, db
 
 app = create_app()
 
 
 class BaseTestCase(TestCase):
     def create_app(self):
-        app.config.from_object('project.config.TestingConfig')
+        app.config.from_object("project.config.TestingConfig")
         return app
 
     def setUp(self):
-        pass
+        db.create_all()
+        db.session.commit()
 
     def tearDown(self):
-        pass
+        db.session.remove()
+        db.drop_all()
