4586b865627b1c0c2e49d6227f95b8f7016ae6eb
Eric Seidler <eric.seidler@gmail.com>
2019-09-18 09:40:26 -0500

    fix typo in docker-push.sh

--- docker-push.sh
+++ docker-push.sh
@@ -41,7 +41,7 @@ then
       docker push $REPO/$USERS_DB:$TAG
     fi
     # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
